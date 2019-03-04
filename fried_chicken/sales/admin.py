aed220da4da8cdbc7f5e1349c55eda9b480112d1
David Castelblanco Benavides <cbdavides@gmail.com>
2019-03-04 18:54:10 -0500

    Made some fields readonly in teh admin.

--- fried_chicken/sales/admin.py
+++ fried_chicken/sales/admin.py
@@ -8,12 +8,16 @@ from .models import TpagaPayment
 class SaleItemInline(admin.TabularInline):
     model = SaleItem
 
+    readonly_fields = ('subtotal', )
+
 
 @admin.register(Sale)
 class SaleAdmin(admin.ModelAdmin):
     inlines = [
         SaleItemInline,
     ]
+
+    readonly_fields = ('order', 'total',)
 
 
 @admin.register(TpagaPayment)
